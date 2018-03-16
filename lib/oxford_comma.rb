abcd4de6bf543cd73a0dc86454a5056927c5f303
Madeline Stark <madeline.stark1@gmail.com>
2018-03-16 00:43:13 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,14 @@
 def oxford_comma(array)
-  last_array = array.pop
   if array.size > 2
+    last_array = array.pop
     array << "and "
     string = array.join(", ")
   elsif array.size > 1
+    last_array = array.pop
     array << "and "
     string = array.join(" ")
   else
+    last_array = array.pop
     string = array.join(" ")
   end
   string << last_array
