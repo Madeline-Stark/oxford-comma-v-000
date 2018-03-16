0fc49623965c552ca75d04d8689e1666f6fa50a1
Madeline Stark <madeline.stark1@gmail.com>
2018-03-16 00:40:40 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,10 @@
 def oxford_comma(array)
   last_array = array.pop
   if array.size > 2
-    array << ", and "
+    array << " and "
     string = array.join(", ")
   elsif array.size > 1
-    array << " and "
+    array << "and "
     string = array.join(" ")
   else
     string = array.join(" ")
